a9e32fc7ef8e0f57c666a82ee7af86152b86ac63
corbin <theseegsjnr@gmail.com>
2018-04-29 09:39:20 -0500

    changed to southwest

--- scraper_test.py
+++ scraper_test.py
@@ -14,35 +14,38 @@ The following code will set up the chromedriver filepath by pulling the
 """
 
 chromedriver = config.get('LocalEnvironment', 'chrome_path')
+print("chromdriver = " + chromedriver)
 os.environ["webdriver.chrome.driver"] = chromedriver
 
 
 # Opens a Chrome window and goes to google.com
 driver = webdriver.Chrome(chromedriver)
-driver.get('http://www.expedia.com/')
+driver.get('https://www.southwest.com/flight/shortcut/low-fare-search.html?int=HOME-BOOKING-WIDGET-ADVANCED-AIR')
 
 # Waits 1 second for the page to load and then goes to the search box
 time.sleep(1);
 
-origin = driver.find_element_by_id("package-origin-hp-package")
-origin.send_keys("ORD")
+origin = driver.find_element_by_id("originAirport_displayed")
+origin.send_keys("mdw")
 origin.send_keys(Keys.RETURN)
 
 time.sleep(1);
 
-destination = driver.find_element_by_id("package-destination-hp-package")
-destination.send_keys("DUR-King Shaka Intl.")
+destination = driver.find_element_by_id("destinationAirport_displayed")
+destination.send_keys("pdx")
 destination.send_keys(Keys.RETURN)
 
 time.sleep(1)
 
-departing = driver.find_element_by_id("package-departing-hp-package")
-departing.send_keys("10/17/2018")
+departing = driver.find_element_by_id("outboundDate")
+departing.send_keys("oct")
 departing.send_keys(Keys.RETURN)
 
+time.sleep(1)
 
-returning = driver.find_element_by_id("package-returning-hp-package")
-returning.send_keys("10/18/2018")
+returning = driver.find_element_by_id("returnDate")
+#returning.clear()
+returning.send_keys("oct")
 returning.send_keys(Keys.RETURN)
 
-searchButton = driver.find_element_by_id('search-button-hp-package')
+searchButton = driver.find_element_by_id('submitButton').click()
